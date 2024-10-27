1992f565a38f5e70e9b5658bb799c2eb3076d3e7
Kibazola <166644753+Kibazola@users.noreply.github.com>
2024-10-27 11:04:51 -0300

    Add files via upload

--- funcoes.py
+++ funcoes.py
@@ -1,12 +1,23 @@
 def define_posicoes(linha, coluna, orientacao, tamanho):
     posicoes = []
     
+    # Adiciona as posições de acordo com a orientação
     if orientacao == "vertical":
         for i in range(tamanho):
             posicoes.append([linha + i, coluna])
-    else:  # Orientação é "horizontal"
+    else:
         for i in range(tamanho):
             posicoes.append([linha, coluna + i])
     
     return posicoes
 
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes_navio = define_posicoes(linha, coluna, orientacao, tamanho)
+    
+    # Se o navio já está no dicionário, adiciona a nova posição
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes_navio)
+    else:
+        frota[nome_navio] = [posicoes_navio]
+    
+    return frota
